d65cd2d532662f335923109c14fac14410e7d184
hudate <hbombdate@163.com>
2019-06-05 19:54:57 +0800

    Create file 'Browser.py', for class Browser.

---
+++ Broswer.py
@@ -0,0 +1,10 @@
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.options import Options
+from time import *
+from getModules import *
+
+
+
+
