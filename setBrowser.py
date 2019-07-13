07c44ad7bcca35845b317dbbbe89922ac9ae92fd
hudate <hbombdate@163.com>
2019-07-13 15:50:20 +0800

    增加了文件解释器和文件编码。

--- setBrowser.py
+++ setBrowser.py
@@ -1,9 +1,10 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.firefox.options import Options
 from time import *
-from getModules import *
 
 
 
